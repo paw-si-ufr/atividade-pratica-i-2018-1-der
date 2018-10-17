fada38005d178c792c732ca51eccda97f4590c4d
Danilorfzk <31334241+Danilorfzk@users.noreply.github.com>
2018-10-17 10:08:36 -0300

    Update dados-cad-evento.php

--- eventos/dados-cad-evento.php
+++ eventos/dados-cad-evento.php
@@ -13,8 +13,15 @@
     $id_usuario = $_SESSION['id_usuario'];
 
       
-    $result = "INSERT INTO evento (titulo, descricao, imagem, data, horario, local, valor, vagas, id_usuario) VALUES ('$titulo', '$descricao', '$imagem', '$data', '$horario', '$local', '$valor', '$vagas', $id_usuario)";
+    $result = "INSERT INTO evento (titulo, descricao, imagem, data, hora, local, valor, vagas, id_usuario) VALUES ('$titulo', '$descricao', '$imagem', '$data', '$horario', '$local', '$valor', '$vagas', '$id_usuario')";
     $result = mysqli_query($conn , $result);
+
+    if($result){
+        header("Location: adm.php");
+    }
+    else{
+        echo "Erro Ocorrido: " .mysqli_error($conn);
+    }
 
 
 
